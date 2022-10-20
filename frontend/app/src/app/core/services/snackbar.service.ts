3653d69c726d2ced97de0deab2949000b29bc7c9
bundulappp <bundulappp@gmail.com>
2022-10-20 16:05:31 +0200

    add snackbar happy path

--- frontend/app/src/app/core/services/snackbar.service.ts
+++ frontend/app/src/app/core/services/snackbar.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SnackbarService {
+  private duration: number = 3000;
+  private actionText: string = 'close';
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) {}
+
+  showSuccessMessage(successMessage: string): void {
+    this.snackBar.open(successMessage, this.actionText, {
+      duration: this.duration,
+      panelClass: ['green-snackbar'],
+    });
+  }
 }

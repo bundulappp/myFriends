1d9e9bbe62107d741c762d6b7c0b1a34c9a23816
bundulappp <bundulappp@gmail.com>
2022-10-20 14:54:46 +0200

    import mat tabs

--- frontend/app/src/app/shared/shared.module.ts
+++ frontend/app/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [CommonModule, RouterModule, HeaderComponent],
+  imports: [CommonModule, RouterModule, MatTabsModule],
+  exports: [CommonModule, RouterModule, HeaderComponent, MatTabsModule],
 })
 export class SharedModule {}

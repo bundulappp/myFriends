2aa53020113f305f3355ec9637faf2d2656221db
bundulappp <bundulappp@gmail.com>
2022-10-20 10:01:04 +0200

    editFriend swagger doc and route added

--- backend/src/routes/friends.routes.ts
+++ backend/src/routes/friends.routes.ts
@@ -42,4 +42,48 @@ const friendsRouter = express.Router();
  */
 friendsRouter.post('', friendController.addNewFriend);
 
+/**
+ * @swagger
+ * /api/friends:
+ *  put:
+ *      tags:
+ *      - FRIENDS
+ *      description: Edit a friend details
+ *      parameters:
+ *          - in: body
+ *            name: friendData
+ *            description: Provide the details of the friend you'd like to edit
+ *            schema:
+ *              type: object
+ *              properties:
+ *                friendId:
+ *                  type: number
+ *                  example: 1
+ *                name:
+ *                  type: string
+ *                  example: John Wick 2
+ *                email:
+ *                  type: string
+ *                  example: jowhnwick2@example.com
+ *                comment:
+ *                  type: string
+ *                  example: it's a great comment
+ *                favFood:
+ *                  type: string
+ *                  example: Mac&Cheese
+ *                relationshipStatus:
+ *                  type: number
+ *                  example: 1
+ *      responses:
+ *          200:
+ *              description: Edited
+ *          400:
+ *              description: Bad request
+ *          404:
+ *              description: Not found
+ *          500:
+ *              description: Internal server error
+ */
+friendsRouter.put('', friendController.editFriend);
+
 export default friendsRouter;

74b8b0f4d69f0e230b1aee5772932d2384379dae
bundulappp <bundulappp@gmail.com>
2022-10-20 15:55:01 +0200

    add material modules

--- frontend/app/src/app/shared/shared.module.ts
+++ frontend/app/src/app/shared/shared.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule, MatTabsModule],
-  exports: [CommonModule, RouterModule, HeaderComponent, MatTabsModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    HeaderComponent,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class SharedModule {}

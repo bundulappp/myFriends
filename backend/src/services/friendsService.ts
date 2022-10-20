389aa140746e7d262c0f64cc08ffbb764f0d6f71
bundulappp <bundulappp@gmail.com>
2022-10-20 09:15:58 +0200

    add editFriend to service layer

--- backend/src/services/friendsService.ts
+++ backend/src/services/friendsService.ts
@@ -1,8 +1,20 @@
+import { EditFriendRequestModel } from '../models/common/EditFriendRequestModel';
 import { AddFriendRequestViewModel } from '../models/view/AddFriendRequestViewModel';
 import { friendRepository } from '../repositories/friends.repository';
+import { notFoundError } from './generalErrorService';
 
 export const friendService = {
   async addNewFriend(friendDetail: AddFriendRequestViewModel): Promise<number> {
     return await friendRepository.addNewFriend(friendDetail);
+  },
+
+  async editFriend(friendDetails: EditFriendRequestModel): Promise<void> {
+    const { friendId } = friendDetails;
+
+    if (!(await friendRepository.getFriendById(friendId))) {
+      throw notFoundError('Cannot edit friend, friendId not found in db!');
+    }
+
+    await friendRepository.editFriendById(friendDetails);
   },
 };

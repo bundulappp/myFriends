43a53b82b27a1acf39bcfd9ee323344119ea9d86
bundulappp <bundulappp@gmail.com>
2022-10-20 20:31:04 +0200

    add routing

--- frontend/app/src/app/features/main-page/main-page-routing.module.ts
+++ frontend/app/src/app/features/main-page/main-page-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
 import { ListComponent } from './list/list.component';
 import { MainPageComponent } from './main-page.component';
 
@@ -7,7 +8,10 @@ const routes: Routes = [
   {
     path: '',
     component: MainPageComponent,
-    children: [{ path: 'list', component: ListComponent }],
+    children: [
+      { path: 'list', component: ListComponent },
+      { path: 'add', component: AddComponent },
+    ],
   },
 ];
 

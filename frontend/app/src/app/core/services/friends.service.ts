9c6166561283cf5e692383bdc357637084d597aa
bundulappp <bundulappp@gmail.com>
2022-10-20 20:27:26 +0200

    add getAllFriends to friendsService

--- frontend/app/src/app/core/services/friends.service.ts
+++ frontend/app/src/app/core/services/friends.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FriendViewModel } from 'src/app/shared/models/FriendViewModel';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FriendsService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getAllFriends(): Observable<FriendViewModel[]> {
+    return this.http.get<FriendViewModel[]>(`${environment.apiUrl}/friends`);
+  }
 }

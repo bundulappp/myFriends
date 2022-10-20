c5def594159a2a92e9e94798cab417e3a7d55c36
bundulappp <bundulappp@gmail.com>
2022-10-20 15:53:59 +0200

    init list component

--- frontend/app/src/app/features/main-page/main-page-routing.module.ts
+++ frontend/app/src/app/features/main-page/main-page-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
 import { MainPageComponent } from './main-page.component';
 
-const routes: Routes = [{ path: '', component: MainPageComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    children: [{ path: 'list', component: ListComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MainPageRoutingModule { }
+export class MainPageRoutingModule {}

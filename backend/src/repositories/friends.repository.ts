c74120b3b68473bf20170ecbd66de08bfde57b54
bundulappp <bundulappp@gmail.com>
2022-10-20 08:50:07 +0200

    add editFriendById method to repository

--- backend/src/repositories/friends.repository.ts
+++ backend/src/repositories/friends.repository.ts
@@ -1,5 +1,6 @@
 import { OkPacket } from 'mysql';
 import { db } from '../data/connection';
+import { RelationshipStatusTypes } from '../models/enums/RelationshipStatusTypes';
 import { AddFriendRequestViewModel } from '../models/view/AddFriendRequestViewModel';
 
 export const friendRepository = {
@@ -23,5 +24,33 @@ export const friendRepository = {
     ]);
 
     return result.insertId;
+  },
+
+  async editFriendById(
+    friendId: number,
+    name: string,
+    email: string,
+    comment: string,
+    favFood: string,
+    relationshipStatus: RelationshipStatusTypes,
+  ): Promise<void> {
+    const query = `UPDATE
+                friends
+              SET
+                name=?,
+                email=?,
+                comment=?,
+                favFood=?,
+                relationshipStatus=?
+              WHERE
+                id=?`;
+    await db.query(query, [
+      name,
+      email,
+      comment,
+      favFood,
+      relationshipStatus.toString(),
+      friendId.toString(),
+    ]);
   },
 };

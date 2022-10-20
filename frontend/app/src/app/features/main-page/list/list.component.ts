15a3ad783542f68230c963cb26874b66dab5702d
bundulappp <bundulappp@gmail.com>
2022-10-20 20:28:33 +0200

    table column display name and list data added

--- frontend/app/src/app/features/main-page/list/list.component.ts
+++ frontend/app/src/app/features/main-page/list/list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FriendsService } from 'src/app/core/services/friends.service';
+import { FriendViewModel } from 'src/app/shared/models/FriendViewModel';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  friendList: FriendViewModel[] = [];
+  columnsToDisplay = [
+    'name',
+    'email',
+    'comment',
+    'favFood',
+    'relationshipStatus',
+    'actions',
+  ];
 
-  constructor() { }
+  constructor(private friendsService: FriendsService) {}
 
   ngOnInit(): void {
+    this.friendsService.getAllFriends().subscribe((x) => {
+      this.friendList = x;
+    });
   }
-
 }

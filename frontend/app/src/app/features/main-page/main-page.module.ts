de3bffebd8d4fc9f01ef6f41e60db41777921375
bundulappp <bundulappp@gmail.com>
2022-10-20 20:30:03 +0200

    init add

--- frontend/app/src/app/features/main-page/main-page.module.ts
+++ frontend/app/src/app/features/main-page/main-page.module.ts
@@ -4,9 +4,10 @@ import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageComponent } from './main-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
-  declarations: [MainPageComponent, ListComponent],
+  declarations: [MainPageComponent, ListComponent, AddComponent],
   imports: [CommonModule, MainPageRoutingModule, SharedModule],
 })
 export class MainPageModule {}

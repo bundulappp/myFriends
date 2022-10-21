32fde2484490fa2c442a32469df38af2d2d060fb
bundulappp <bundulappp@gmail.com>
2022-10-21 08:27:46 +0200

    add validators and rel.status

--- frontend/app/src/app/features/main-page/add/add.component.ts
+++ frontend/app/src/app/features/main-page/add/add.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FriendsService } from 'src/app/core/services/friends.service';
+import { RelationshipStatusTypes } from 'src/app/shared/models/enums/RelationshipStatusTypes';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
-export class AddComponent implements OnInit {
+export class AddComponent {
+  relationshipStatus = [
+    { id: RelationshipStatusTypes.Single, name: 'single' },
+    { id: RelationshipStatusTypes.In_relationship, name: 'in relationship' },
+    { id: RelationshipStatusTypes.Married, name: 'married' },
+  ];
 
-  constructor() { }
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    comment: new FormControl('', Validators.maxLength(30)),
+    favFood: new FormControl('', Validators.required),
+    relationshipStatus: new FormControl('', Validators.required),
+  });
 
-  ngOnInit(): void {
+  constructor(private friendsService: FriendsService, private router: Router) {}
+
+  addFriend() {}
+
+  back(): void {
+    this.router.navigate(['/main']);
   }
-
 }

61def14b5dce40b3770f919777a103b34e085d40
bundulappp <bundulappp@gmail.com>
2022-10-21 09:30:58 +0200

    addNewFriend service method added

--- frontend/app/src/app/core/services/friends.service.ts
+++ frontend/app/src/app/core/services/friends.service.ts
@@ -2,14 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FriendViewModel } from 'src/app/shared/models/FriendViewModel';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+import { AddNewFriendRequestViewModel } from 'src/app/shared/models/AddNewFriendViewModel';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FriendsService {
   constructor(private http: HttpClient) {}
+
   getAllFriends(): Observable<FriendViewModel[]> {
     return this.http.get<FriendViewModel[]>(`${environment.apiUrl}/friends`);
+  }
+
+  addNewFriend(newItemObject: AddNewFriendRequestViewModel): Observable<void> {
+    return this.http
+      .post<AddNewFriendRequestViewModel>(
+        `${environment.apiUrl}/friends`,
+        newItemObject
+      )
+      .pipe(map(() => undefined));
   }
 }

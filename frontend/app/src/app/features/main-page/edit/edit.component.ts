949b397831b0fbd44c4c8588a242a9804c12e053
bundulappp <bundulappp@gmail.com>
2022-10-22 11:25:34 +0200

    add from control

--- frontend/app/src/app/features/main-page/edit/edit.component.ts
+++ frontend/app/src/app/features/main-page/edit/edit.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss']
+  styleUrls: ['./edit.component.scss'],
 })
-export class EditComponent implements OnInit {
+export class EditComponent {
+  relationshipCategory = [
+    { id: 0, name: 'single' },
+    { id: 1, name: 'in relationship' },
+    { id: 2, name: 'married' },
+  ];
 
-  constructor() { }
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    comment: new FormControl('', Validators.maxLength(30)),
+    favFood: new FormControl('', Validators.required),
+    relationshipStatus: new FormControl('', Validators.required),
+  });
 
-  ngOnInit(): void {
+  constructor() {}
+
+  editFriend(): void {}
+
+  back(): void {}
+
+  get name(): AbstractControl {
+    return this.form.get('name') as AbstractControl;
   }
 
+  get email(): AbstractControl {
+    return this.form.get('email') as AbstractControl;
+  }
+
+  get comment(): AbstractControl {
+    return this.form.get('comment') as AbstractControl;
+  }
+
+  get favFood(): AbstractControl {
+    return this.form.get('favFood') as AbstractControl;
+  }
+
+  get relationshipStatus(): AbstractControl {
+    return this.form.get('relationshipStatus') as AbstractControl;
+  }
 }

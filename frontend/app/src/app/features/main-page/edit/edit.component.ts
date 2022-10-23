e408d6b314c58ee4d51db94bef1d9767907ab51a
bundulappp <bundulappp@gmail.com>
2022-10-23 14:09:06 +0200

    get data from db and filled up the form

--- frontend/app/src/app/features/main-page/edit/edit.component.ts
+++ frontend/app/src/app/features/main-page/edit/edit.component.ts
@@ -5,13 +5,18 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { FriendsService } from 'src/app/core/services/friends.service';
+import { FriendViewModel } from 'src/app/shared/models/FriendViewModel';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss'],
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
+  friendDetails: FriendViewModel;
+
   relationshipCategory = [
     { id: 0, name: 'single' },
     { id: 1, name: 'in relationship' },
@@ -26,9 +31,31 @@ export class EditComponent {
     relationshipStatus: new FormControl('', Validators.required),
   });
 
-  constructor() {}
+  constructor(
+    private friendsService: FriendsService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  editFriend(): void {}
+  ngOnInit(): void {
+    this.activatedRoute.queryParamMap.subscribe((params) => {
+      this.init(Number(params.get('friendId')));
+    });
+  }
+
+  init(friendId: number): void {
+    this.friendsService.getFriendDetails(friendId).subscribe((x) => {
+      this.friendDetails = x;
+
+      const comment = x['comment'] as string;
+      const relationshipStatus = x['relationshipStatus'] as string;
+      const { name, email, favFood } = x;
+      this.form.setValue({ name, email, comment, favFood, relationshipStatus });
+    });
+  }
+
+  editFriend(): void {
+    console.log(this.friendDetails);
+  }
 
   back(): void {}
 

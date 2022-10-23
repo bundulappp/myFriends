9eece1d5c2669f638d9639ab54c5c150f6fc6ca8
bundulappp <bundulappp@gmail.com>
2022-10-23 13:49:22 +0200

    addFriendIToQueryParams added

--- frontend/app/src/app/features/main-page/list/list.component.ts
+++ frontend/app/src/app/features/main-page/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FriendsService } from 'src/app/core/services/friends.service';
 import { FriendViewModel } from 'src/app/shared/models/FriendViewModel';
 
@@ -9,6 +10,7 @@ import { FriendViewModel } from 'src/app/shared/models/FriendViewModel';
 })
 export class ListComponent implements OnInit {
   friendList: FriendViewModel[] = [];
+
   columnsToDisplay = [
     'name',
     'email',
@@ -24,7 +26,7 @@ export class ListComponent implements OnInit {
     { name: 'married' },
   ];
 
-  constructor(private friendsService: FriendsService) {}
+  constructor(private friendsService: FriendsService, private router: Router) {}
 
   ngOnInit(): void {
     this.friendsService.getAllFriends().subscribe((friendList) => {
@@ -46,11 +48,15 @@ export class ListComponent implements OnInit {
     });
   }
 
-  deleteFriend(friendId: number): void {
-    this.friendsService.deleteFriend(friendId).subscribe();
+  addFriendIToQueryParams(friendId: number): void {
+    this.router.navigate(['/main/edit'], {
+      queryParams: {
+        friendId: `${friendId}`,
+      },
+    });
   }
 
-  editFriend(friendId: number): void {
-    console.log(friendId);
+  deleteFriend(friendId: number): void {
+    this.friendsService.deleteFriend(friendId).subscribe();
   }
 }

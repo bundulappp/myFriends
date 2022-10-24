e038bae84cb22091bac89cd88488c186c12be3af
bundulappp <bundulappp@gmail.com>
2022-10-24 16:05:29 +0200

    get friend data from db

--- frontend/app/src/app/features/main-page/view/view.component.ts
+++ frontend/app/src/app/features/main-page/view/view.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FriendsService } from 'src/app/core/services/friends.service';
+import { FriendViewModel } from 'src/app/shared/models/FriendViewModel';
 
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
-  styleUrls: ['./view.component.scss']
+  styleUrls: ['./view.component.scss'],
 })
 export class ViewComponent implements OnInit {
+  friendDetails: FriendViewModel;
 
-  constructor() { }
+  constructor(
+    private friendsService: FriendsService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.queryParamMap.subscribe((params) => {
+      console.log(params);
+      this.init(Number(params.get('friendId')));
+    });
   }
 
+  init(friendId: number): void {
+    this.friendsService.getFriendDetails(friendId).subscribe((x) => {
+      this.friendDetails = x;
+    });
+  }
 }

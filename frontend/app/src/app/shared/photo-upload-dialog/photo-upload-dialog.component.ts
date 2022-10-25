a2ab1b49d1ee2298365e70427641e9f4b987c363
bundulappp <bundulappp@gmail.com>
2022-10-25 13:25:34 +0200

    implement editPhoto method

--- frontend/app/src/app/shared/photo-upload-dialog/photo-upload-dialog.component.ts
+++ frontend/app/src/app/shared/photo-upload-dialog/photo-upload-dialog.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FriendsService } from 'src/app/core/services/friends.service';
+import { DialogFriendDataViewModel } from '../models/DialogFriendDataViewModel';
 
 @Component({
   selector: 'app-photo-upload-dialog',
   templateUrl: './photo-upload-dialog.component.html',
-  styleUrls: ['./photo-upload-dialog.component.scss']
+  styleUrls: ['./photo-upload-dialog.component.scss'],
 })
-export class PhotoUploadDialogComponent implements OnInit {
+export class PhotoUploadDialogComponent {
+  @Input() photoUrl: string;
+  constructor(
+    private friendsService: FriendsService,
+    @Inject(MAT_DIALOG_DATA) public data: DialogFriendDataViewModel
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  editPhoto(): void {
+    const friendData = {
+      friendId: this.data.friendId,
+      photoUrl: this.photoUrl,
+    };
+    this.friendsService.editFriendPhoto(friendData).subscribe();
   }
-
 }

c8f81b46a0402633c078de8c7362c5e35b6cb308
bundulappp <bundulappp@gmail.com>
2022-10-25 12:33:09 +0200

    add openDialog method

--- frontend/app/src/app/features/main-page/view/view.component.ts
+++ frontend/app/src/app/features/main-page/view/view.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { FriendsService } from 'src/app/core/services/friends.service';
 import { FriendViewModel } from 'src/app/shared/models/FriendViewModel';
+import { PhotoUploadDialogComponent } from 'src/app/shared/photo-upload-dialog/photo-upload-dialog.component';
 
 @Component({
   selector: 'app-view',
@@ -13,7 +15,8 @@ export class ViewComponent implements OnInit {
 
   constructor(
     private friendsService: FriendsService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -26,5 +29,15 @@ export class ViewComponent implements OnInit {
     this.friendsService.getFriendDetails(friendId).subscribe((x) => {
       this.friendDetails = x;
     });
+  }
+
+  openDialog(): void {
+    const dialog = this.dialog.open(PhotoUploadDialogComponent, {
+      data: {
+        friendId: this.friendDetails.id,
+      },
+    });
+
+    dialog.afterClosed().subscribe();
   }
 }
